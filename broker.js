dec4447271ad7d1e3c3cae3c83c43cafb7d69db2
Filiplinjo <filiphe.stud@ntnu.no>
2021-04-08 22:50:36 +0200

    broker funker mot mongodb

--- broker.js
+++ broker.js
@@ -1,22 +1,16 @@
 // MQTT broker
 var mosca = require('mosca')
 var settings = {port: 8080}
-//const PORT = process.env.PORT || 8080
 var broker = new mosca.Server(settings)
 
 // MongoDB
 var MongoClient = require('mongodb').MongoClient
-//const MONGODB_URI="mongodb+srv://filip.h.eikenes@gmail.com:cloud98reddragon@cluster0.zhaw6.mongodb.net/project1?retryWrites=true&w=majority"; // Your connection string
-const uri = "mongodb+srv://filip.h.eikenes@gmail.com:cloud98reddragon@cluster0.zhaw6.mongodb.net/project1?retryWrites=true&w=majority";
-//const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://dbadmin:81234Witg@cluster0.zhaw6.mongodb.net/project1?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
 
 async function run() {
   try {
     await client.connect();
-
-   // const collection = client.db("project1").collection("home");
 
     broker.on('ready', ()=>{
     console.log('Broker is ready!')
@@ -28,7 +22,6 @@ async function run() {
 
     //MongoClient.connect(uri, (error, client)=>{
     var myCol = client.db("project1").collection("home") // DB name and collection name
-   // const collection = client.db("project1").collection("home");
     myCol.insertOne({
     message:message
     }, ()=>{
