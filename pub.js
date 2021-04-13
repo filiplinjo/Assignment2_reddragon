1c1f4f942447405ca60b044bec6580d671da2064
Filiplinjo <filiphe.stud@ntnu.no>
2021-04-13 13:28:54 +0200

    la til sensor2

--- pub.js
+++ pub.js
@@ -10,6 +10,8 @@ function random() {
 
 //Number 1 sensor (light)
 var topic1 = 'light'
+var topic2 = 'proxmity'
+
 
 function light() {
   var message1 = '{"Data":{"SOM":{"Tab":[{"Values":{"SensorID":"light","value": '+random()+'}}]}}}';
@@ -19,15 +21,30 @@ function light() {
   return message1;
 }
 
+
+function proxmity() {
+  var message1 = '{"Data":{"SOM":{"Tab":[{"Values":{"SensorID":"proxmitiy","value": '+random()+'}}]}}}';
+  var jsonObj = JSON.parse(message1);
+  message1 = json2xml(jsonObj);
+  console.log(message1);
+  return message1;
+}
+
+
 client.on('connect',()=>{
- //var messageno1 = light();
  setInterval(()=>{
  client.publish(topic1, light())
  console.log('message sent', light())
- },5000)
+ },10000)
 })
 
 
+client.on('connect',()=>{
+ setInterval(()=>{
+ client.publish(topic2, proxmity())
+ console.log('message sent', proxmity())
+ },10000)
+})
 
 
 //NB, bør egentlig flytte json2xml konvertinger til broker.js
