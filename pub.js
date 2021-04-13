fa0d4bbf81c646c17c74d7c4cd56ce950b864197
Erik Øhrling Sørli <erikoso@stud.ntnu.no>
2021-04-13 14:39:46 +0200

    Fiksa opp i feil kalling på funksjoner i connection til db

--- pub.js
+++ pub.js
@@ -85,30 +85,30 @@ client.on('connect',()=>{
 
 client.on('connect',()=>{
  setInterval(()=>{
- client.publish(topic3, proxmity())
- console.log('message sent', proxmity())
+ client.publish(topic3, temperature())
+ console.log('message sent', temperature())
  },10000)
 })
 
 
 client.on('connect',()=>{
  setInterval(()=>{
- client.publish(topic4, proxmity())
- console.log('message sent', proxmity())
+ client.publish(topic4, security())
+ console.log('message sent', security())
  },10000)
 })
 
 client.on('connect',()=>{
  setInterval(()=>{
- client.publish(topic5, proxmity())
- console.log('message sent', proxmity())
+ client.publish(topic5, Smoke_gas())
+ console.log('message sent', Smoke_gas())
  },10000)
 })
 
 client.on('connect',()=>{
  setInterval(()=>{
- client.publish(topic6, proxmity())
- console.log('message sent', proxmity())
+ client.publish(topic6, Humidity())
+ console.log('message sent', Humidity())
  },10000)
 })
 //NB, bør egentlig flytte json2xml konvertinger til broker.js
